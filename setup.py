61bb1c51de12b699bbb573460443067868b8f054
Mathew Seng <mathewseng@gmail.com>
2020-11-20 08:50:16 -0600

    ENH: Update python package version for modules This will bump the package version in setup.py from 1.0.6 to 1.0.7

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-higherorderaccurategradient',
-    version='1.0.6',
+    version='1.0.7',
     author='Matthew M. McCormick',
     author_email='matt.mccormick@kitware.com',
     packages=['itk'],
